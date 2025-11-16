1ec6f31e429c6e210f2b9c9eb5ad542de58777d7
ElielRegiani <elielregiani@gmail.com>
2025-11-16 12:49:35 -0300

    first commit

---
+++ src/main/kotlin/investiment/message/banking/process/BankingprocessApplication.kt
@@ -0,0 +1,11 @@
+package investiment.message.banking.process
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankingprocessApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankingprocessApplication>(*args)
+}
